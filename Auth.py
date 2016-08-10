8209aab5e3d9d00dca0ff437790b9a9bf25c3aa4
Matthew Page <matthewpagecs@gmail.com>
2016-08-09 20:10:20 -0400

    Added /auth/<token>

---
+++ Auth.py
@@ -0,0 +1,5 @@
+import json, requests
+
+def verifyAuthCode(authcode):
+    url = 'https://graph.accountkit.com/v1.0/access_token?grant_type=authorization_code&code=' + authcode + '&access_token=AA|' + 'id' + '|' + 'app_secret'
+    return requests.get(url).text
--- HelloMessengerAPI.py
+++ HelloMessengerAPI.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+import Auth
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/auth/<authcode>')
+def hello_world(authcode):
+    return Auth.verifyAuthCode(authcode);
 
 
 if __name__ == '__main__':
