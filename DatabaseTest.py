2a7894b5f290f1180a4370dcf0c06b7d842849b7
Matthew Page <matthewpagecs@gmail.com>
2016-08-21 09:15:50 -0500

    Added routes

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -1,11 +1,10 @@
 def test_database(cursor):
-    cursor.execute("insert into auth values ('id', 'token')")
-    cursor.execute("select id from auth where token = 'token';")
-    return cursor.fetchone()
+    cursor.execute("select @@autocommit")
+    return str(cursor.fetchone())
 
 def test_add(cursor):
     cursor.execute("insert into auth values ('id1', 'token1')")
-    return "success!"
+    return test_database(cursor)
 
 def test_get(cursor):
     cursor.execute("select id from auth where token = 'token1'")
--- HelloMessengerAPI.py
+++ HelloMessengerAPI.py
@@ -6,31 +6,80 @@ import GeneralDatabase
 import Secure
 
 app = Flask(__name__)
-db = MySQLdb.connect(Secure.host, Secure.database, Secure.password, Secure.user)
+db = MySQLdb.connect(Secure.host, Secure.user, Secure.password, Secure.database, autocommit=1)
 
-def getDatabaseCursor():
+
+def get_database_cursor():
     cursor = db.cursor()
     return cursor
 
+
 @app.route('/testdatabase')
 def test():
-    return DatabaseTest.test_database(getDatabaseCursor())
+    return DatabaseTest.test_database(get_database_cursor())
+
 
 @app.route('/testadd')
 def testAdd():
-    return DatabaseTest.test_add(getDatabaseCursor())
+    return DatabaseTest.test_add(get_database_cursor())
+
 
 @app.route('/testget')
 def testGet():
-    return DatabaseTest.test_get(getDatabaseCursor())
+    return DatabaseTest.test_get(get_database_cursor())
 
-@app.route('/auth/<authcode>')
-def hello_world(authcode):
-    return Auth.verify_auth_code(authcode, getDatabaseCursor());
 
 @app.route('/reset')
 def reset():
-    return GeneralDatabase.reset_database(getDatabaseCursor())
+    return GeneralDatabase.reset_database(get_database_cursor())
+
+
+@app.route('/auth/accessToken/<authcode>', methods=['GET'])
+def auth_access_token(authcode):
+    return Auth.verify_auth_code(authcode, get_database_cursor());
+
+
+@app.route('/user', methods=['POST', 'PUT', 'DELETE'])
+def users():
+    # TODO
+    return
+
+
+@app.route('/user/<id>', methods=['GET'])
+def user(id):
+    # TODO
+    return
+
+
+@app.route('/conversations/list', methods=['GET'])
+def conversations_list():
+    # TODO
+    return
+
+
+@app.route('/conversations/request', methods=['POST', 'GET', 'DELETE'])
+def conversations_request():
+    # TODO
+    return
+
+
+@app.route('/conversations/mesages/<id>', methods=['POST', 'GET'])
+def conversations_messages(id):
+    # TODO
+    return
+
+
+@app.route('/conversations/leave/<id>', methods=['DELETE'])
+def conversations_messages(id):
+    # TODO
+    return
+
+
+@app.route('/conversations/friend_request/<id>', methods=['POST'])
+def conversations_friend_request(id):
+    # TODO
+    return
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
